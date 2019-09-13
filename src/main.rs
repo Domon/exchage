74ea5a7a877ba556a4e9400389622476513aca1f
Chun-wei Kuo <Dendoh@gmail.com>
2019-09-13 15:05:18 +1000

    Install reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+extern crate reqwest;
+
+use std::collections::HashMap;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp: HashMap<String, String> = reqwest::get("https://httpbin.org/ip")?
+        .json()?;
+    println!("{:#?}", resp);
+    Ok(())
 }
