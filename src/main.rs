e63ca99588c9bf90c73ad27c51d81d642d2910bd
Chun-wei Kuo <Dendoh@gmail.com>
2019-09-13 15:45:35 +1000

    Get currency pair from command line arguments

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,17 @@ use std::collections::HashMap;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let api_key = std::env::var("API_KEY").expect("Environment variable API_KEY is not set");
-    let given_amount = std::env::args().nth(1).expect("Base amount is not given");
+    let given_amount = std::env::args().nth(1).expect("Base amount is not given, e.g. 1.23");
+    let pair = std::env::args().nth(2).expect("Currency pair is not given, e.g. USD_AUD");
 
-    let q = "USD_AUD";
     let url = format!(
-        "https://free.currconv.com/api/v7/convert?q={q}&compact=ultra&apiKey={api_key}",
-        q = q,
+        "https://free.currconv.com/api/v7/convert?q={pair}&compact=ultra&apiKey={api_key}",
+        pair = pair,
         api_key = api_key
     );
 
     let response: HashMap<String, f64> = reqwest::get(&url)?.json()?;
-    let rate = response[q];
+    let rate = response[&pair];
 
     let base_amount: f64 = given_amount.parse().unwrap();
     let target_amount = base_amount * rate;
